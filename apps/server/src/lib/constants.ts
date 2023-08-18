2620408644fa8c97342863c0ac245f13991509f0
eduim <eduardo.im92@gmail.com>
2023-08-18 17:47:23 +0200

    refactor/server: add warn constants DB_URL

--- apps/server/src/lib/constants.ts
+++ apps/server/src/lib/constants.ts
@@ -1,8 +1,14 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 if (!process.env.PORT) {
-  throw new Error("missing PORT in .env file");
+  throw new Error('missing PORT in .env file')
 }
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('missing DATABASE_URL in .env file')
+}
+
+export const DATABASE_URL = process.env.DATABASE_URL
