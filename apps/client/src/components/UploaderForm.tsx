a82df4862e01d16c558c9df9d5e41e65f6f40003
eduim <eduardo.im92@gmail.com>
2023-08-22 16:46:05 +0200

    fix/client: UI components import

--- apps/client/src/components/UploaderForm.tsx
+++ apps/client/src/components/UploaderForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/Input";
-import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function UploaderForm() {
   const [emailTo, setEmailTo] = useState<string>("");
