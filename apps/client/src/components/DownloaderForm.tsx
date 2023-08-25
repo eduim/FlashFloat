290e132bdc196feadac41578dee9eab9252a691d
Albert Genis <albert.SE.work@gmail.com>
2023-08-25 14:27:45 +0200

    download(file): download files from s3

---
+++ apps/client/src/components/DownloaderForm.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function DownloaderForm() {
+  const [uploadId, setUploadId] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUploadId(event.target.value);
+  };
+
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/download/${uploadId}`,
+        {
+          method: "GET",
+        }
+      );
+
+      if (response.ok) {
+        const blob = await response.blob();
+        const fileName = `${uploadId}`;
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        URL.revokeObjectURL(url);
+      } else {
+        console.error("Error downloading file");
+      }
+    } catch (error) {
+      console.error("Error downloading file:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Download Files</h2>
+      <label>
+        Enter Upload Id:
+        <input type="text" value={uploadId} onChange={handleInputChange} />
+      </label>
+      <button onClick={handleDownload}>Download File</button>
+    </div>
+  );
+}
+
+export default DownloaderForm;
