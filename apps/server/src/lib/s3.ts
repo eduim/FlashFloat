beb712ddc1d6e9a93a9d99ed23f8ad0cd417ed16
Eugene Sakwa <the.ardent.arian@gmail.com>
2023-08-25 14:31:46 +0200

    feat/prisma/sever - migrate

--- apps/server/src/lib/s3.ts
+++ apps/server/src/lib/s3.ts
@@ -1,7 +1,12 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { BUCKET_NAME } from './constants'
 
-const client = new S3Client({})
+const client = new S3Client({
+  credentials: {
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+  },
+})
 
 const s3 = {
   async upload(key: string, Body: Buffer) {
