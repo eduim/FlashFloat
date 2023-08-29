8a70b63c058e50aed4324cd34279101ec78a87db
eduim <eduardo.im92@gmail.com>
2023-08-29 11:55:54 +0200

    download/server: multiple files in zip

--- apps/server/src/controllers/downloads.controllers.ts
+++ apps/server/src/controllers/downloads.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import downloadModel from '../models/download'
 import s3 from '../lib/s3'
+import archiver from 'archiver'
 
 const downloadController = {
   async download(req: Request, res: Response) {
@@ -16,20 +17,21 @@ const downloadController = {
         throw new Error('File not found')
       }
 
+      const zip = archiver('zip')
+      zip.pipe(res)
+
       for (const file of files) {
         const fileBuffer = await s3.download(file.path)
 
         if (fileBuffer) {
-          res.set('Content-Type', file.typeOfFile)
-          res.set(
-            'Content-Disposition',
-            `attachment; filename = "${file.fileName}"`
-          )
-          res.send(fileBuffer)
+          zip.append(fileBuffer, {
+            name: file.fileName,
+          })
         } else {
           throw new Error('Error downloading file')
         }
       }
+      zip.finalize()
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ message: error.message })
