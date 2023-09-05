7b3870e95fa5a5fb66f61eff2f5933aab0caddd8
Arshia Abedi <arshiaabedi@Arshias-MacBook-Air.local>
2023-09-05 13:05:10 +0200

    card(vertical & horizontal): added design to vertical and horizontal

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,21 +1,37 @@
-import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import VerticalCard from "./components/VerticalCard"; // Import the VerticalCard component
 import UploaderForm from "./components/UploaderForm";
 import DownloadPage from "./components/DownloadPage";
 import UploadConfirmation from "./components/UploadConfirmation";
+import HorizontalCard from "./components/HorizontalCard";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <UploaderForm />,
+    element: (
+      <div>
+        <VerticalCard x={310} y={171} w={446} h={640}>
+          <UploaderForm />
+        </VerticalCard>
+      </div>
+    ),
   },
+
   {
     path: "/confirmation",
-    element: <UploadConfirmation />,
+    element: (
+      <HorizontalCard>
+        <UploadConfirmation />
+      </HorizontalCard>
+    ),
   },
   {
     path: "/download/:uploadId",
-    element: <DownloadPage />,
+    element: (
+      <HorizontalCard x={310} y={171} w={1000} h={640}>
+        <DownloadPage />
+      </HorizontalCard>
+    ),
   },
 ]);
 
@@ -24,5 +40,5 @@ export default function App() {
     <div className="bg-app-background bg-cover  bg-no-repeat bg-center min-h-screen">
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
--- apps/client/src/components/HorizontalCard.tsx
+++ apps/client/src/components/HorizontalCard.tsx
@@ -2,10 +2,31 @@ import React from "react";
 
 interface CardProps {
   children: React.ReactNode;
+  x?: number; // x-coordinate
+  y?: number; // y-coordinate
+  w?: number; // width
+  h?: number; // height
 }
 
-const HorizontalCard: React.FC<CardProps> = ({ children }) => {
-  return <div className="card">{children}</div>;
+const HorizontalCard: React.FC<CardProps> = ({ children, x, y, w, h }) => {
+  const cardStyle: React.CSSProperties = {
+    position: "absolute",
+    left: x ? `${x}px` : "0", // Default to 0 if x is not provided
+    top: y ? `${y}px` : "0", // Default to 0 if y is not provided
+    width: w ? `${w}px` : "100%", // Default to 100% width if w is not provided
+    height: h ? `${h}px` : "100%", // Default to 100% height if h is not provided
+    background: "black", // Set background color to black
+    color: "white", // Set text color to white
+    border: "1px solid #ccc", // Example border style
+    padding: "16px", // Example padding
+  };
+
+  return (
+    <div className="card" style={cardStyle}>
+      {children}
+    </div>
+  );
 };
 
 export default HorizontalCard;
+
