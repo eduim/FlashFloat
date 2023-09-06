3418e4b81c75599e2cfb7dce7edac4c3a4419087
Arshia Abedi <arshiaabedi@Arshias-MacBook-Air.local>
2023-09-06 12:44:29 +0200

    Layout(cards): updating the styling for the cards and layouts

--- apps/client/src/components/HorizontalCard.tsx
+++ apps/client/src/components/HorizontalCard.tsx
@@ -2,31 +2,18 @@ import React from "react";
 
 interface CardProps {
   children: React.ReactNode;
-  x?: number; // x-coordinate
-  y?: number; // y-coordinate
-  w?: number; // width
-  h?: number; // height
+  w?: string;
+  h?: string;
 }
 
-const HorizontalCard: React.FC<CardProps> = ({ children, x, y, w, h }) => {
-  const cardStyle: React.CSSProperties = {
-    position: "absolute",
-    left: x ? `${x}px` : "0", // Default to 0 if x is not provided
-    top: y ? `${y}px` : "0", // Default to 0 if y is not provided
-    width: w ? `${w}px` : "100%", // Default to 100% width if w is not provided
-    height: h ? `${h}px` : "100%", // Default to 100% height if h is not provided
-    background: "black", // Set background color to black
-    color: "white", // Set text color to white
-    border: "1px solid #ccc", // Example border style
-    padding: "16px", // Example padding
-  };
+const HorizontalCard: React.FC<CardProps> = ({
+  children,
+  w = "w-4/5", // Adjust the width for horizontal card
+  h = "h-100",
+}) => {
+  const cardClasses = `bg-black text-white border-2 border-gray-700 p-4 ${w} ${h}`;
 
-  return (
-    <div className="card" style={cardStyle}>
-      {children}
-    </div>
-  );
+  return <div className={cardClasses}>{children}</div>;
 };
 
 export default HorizontalCard;
-
