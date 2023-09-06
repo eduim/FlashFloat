873b2c97b48952dc45c8b5fa277d1e34aed20867
Eugene Sakwa <the.ardent.arian@gmail.com>
2023-09-06 13:24:43 +0200

    Changes for get route

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -6,6 +6,10 @@ const upload = multer({ storage: multer.memoryStorage() })
 
 const router = express.Router()
 
+router.get('/', (_, res) => {
+  return res.status(200).json('hello')
+})
+
 router.post('/upload', upload.array('fileUpload'), uploadController.upload)
 
 router.get('/download/:uploadId', uploadController.download)
