afccb23bf202216f24fe72e44397d646f674a272
Eugene Sakwa <the.ardent.arian@gmail.com>
2023-09-06 10:50:35 +0200

    Add transer details and the ellipse to the download page

--- apps/client/src/components/DownloadPage.tsx
+++ apps/client/src/components/DownloadPage.tsx
@@ -1,18 +1,20 @@
 import { server } from "@/lib/constants";
 import { useParams } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
+import { dateFormatter, fileFormatter } from "@/lib/formatter";
+import ellipse from "../assets/Ellipse 1.png";
 
 function DownloadPage() {
   const { uploadId } = useParams();
+  const location = useLocation();
+  const { yourEmail, title, message, numberFiles, totalSize, expiresAt } =
+    location.state || {};
 
   const handleDownload = async () => {
     try {
-      const response = await fetch(
-        `${server}/download/${uploadId}`,
-        {
-          method: "GET",
-        }
-      );
+      const response = await fetch(`${server}/download/${uploadId}`, {
+        method: "GET",
+      });
 
       if (response.ok) {
         const blob = await response.blob();
@@ -33,11 +35,34 @@ function DownloadPage() {
 
   return (
     <div>
-      <h2>Download Files</h2>
-      <button onClick={handleDownload}>Download File</button>
+      <div>
+        <div>
+          <p>Your transfer details</p>
+          <p>
+            {" "}
+            {fileFormatter(totalSize)} .
+            {numberFiles === 1 ? `${numberFiles} File` : `${numberFiles} Files`}
+            . Expires {dateFormatter(new Date(expiresAt))}
+          </p>
+          <p>Sent From</p>
+          <p>{yourEmail}</p>
+          <p>Title</p>
+          <p>{title}</p>
+          <p>Message</p>
+          <p>{message}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center w-448 h-640 bg-gray-800 bg-opacity-90">
+        <div>
+          <img src={ellipse} alt="" />
+        </div>
+        <div>
+          <h2>Download Files</h2>
+          <button onClick={handleDownload}>Download File</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default DownloadPage;
-
