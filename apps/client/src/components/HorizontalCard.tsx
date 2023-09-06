11c237c687d1a63c7a856e088fd35b7a1731f7d0
Arshia Abedi <arshiaabedi@Arshias-MacBook-Air.local>
2023-09-06 12:51:55 +0200

    Layout(deleted my horizontal card): removed my code for horizontal card

--- apps/client/src/components/HorizontalCard.tsx
+++ apps/client/src/components/HorizontalCard.tsx
@@ -1,19 +1,1 @@
-import React from "react";
 
-interface CardProps {
-  children: React.ReactNode;
-  w?: string;
-  h?: string;
-}
-
-const HorizontalCard: React.FC<CardProps> = ({
-  children,
-  w = "w-4/5", // Adjust the width for horizontal card
-  h = "h-100",
-}) => {
-  const cardClasses = `bg-black text-white border-2 border-gray-700 p-4 ${w} ${h}`;
-
-  return <div className={cardClasses}>{children}</div>;
-};
-
-export default HorizontalCard;
